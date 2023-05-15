be8e7573bab3fc34d794b1f9eead7f3af0efa6aa
Hikmat Asgarli <hikmatasgarli@gmail.com>
2023-05-15 14:30:30 +0400

    Arrow Added IMG

--- src/components/Advertisement/PopularRest.jsx
+++ src/components/Advertisement/PopularRest.jsx
@@ -3,10 +3,21 @@ import kfc from '../../assets/popular/kfc.avif'
 import pap from '../../assets/popular/pap.avif'
 import bir from '../../assets/popular/bir.avif'
 import friday from '../../assets/popular/friday.avif'
+import kafecity from '../../assets/popular/caf.avif'
+import shaurma from '../../assets/popular/shaur.avif'
+import tendir from '../../assets/popular/tendir.avif'
 
+import left from '../../assets/icons/left-arrow.png'
+import right from '../../assets/icons/right-arrow.png'
 const PopularRest = () => {
     return(
-        <div className="pop__wrapper">
+        <div className="pop__container">
+            <div className="right-left">
+                <img src={left}/>
+                <img  src={right}/>
+            </div>
+            <div className="pop__wrapper">
+            
             <div className="box">
                 <img src={mac}/>
                 <div className="box__title">McDonald's</div>
@@ -27,6 +38,19 @@ const PopularRest = () => {
                 <img src={friday}/>
                 <div className="box__title">FRYDAY</div>
             </div>
+            <div className="box">
+                <img src={kafecity}/>
+                <div className="box__title">CafeCity</div>
+            </div>
+            <div className="box">
+                <img src={shaurma}/>
+                <div className="box__title">Shaurma №1</div>
+            </div>
+            <div className="box">
+                <img src={tendir}/>
+                <div className="box__title">Tendir & Mendir</div>
+            </div>
+        </div>
         </div>
     )
 }
